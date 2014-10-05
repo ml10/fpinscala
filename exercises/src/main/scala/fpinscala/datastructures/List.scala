40e109fa0345699301ee2c41f2cecf0c76853d7d
Andrew Hamilton <andrew.hamil10@gmail.com>
2014-10-05 11:48:05 -0500

    foldLeft impl

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -87,7 +87,22 @@ object List { // `List` companion object. Contains functions for creating and wo
   def length[A](l: List[A]): Int = 
     foldRight(l, 0)((_, y) => y + 1)
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
+  @annotation.tailrec
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Nil => z
+    case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+  }
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
+  def sum3(ns: List[Int]) = 
+    foldLeft(ns, 0)(_ + _)
+  
+  def product3(ns: List[Double]) = 
+    foldLeft(ns, 1.0)(_ * _)
+
+  def length2[A](l: List[A]): Int = 
+    foldLeft(l, 0)((x, _) => x + 1)
+
+  def map[A,B](l: List[A])(f: A => B): List[B] = {
+    foldRight[A, List[B]](l, Nil)((x, y) => Cons(f(x), y))
+  }
 }
